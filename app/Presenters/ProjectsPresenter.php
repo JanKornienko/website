66821c4b693f141de6a6cf3287262ce4ef6699b0
Jan Kornienko <kor.jan@outlook.com>
2021-12-06 21:27:28 +0100

    Add file

--- app/Presenters/ProjectsPresenter.php
+++ app/Presenters/ProjectsPresenter.php
@@ -7,6 +7,7 @@ namespace App\Presenters;
 use Nette;
 use Nette\Database\Explorer;
 use Nette\Application\UI\Form;
+use Nette\Utils\FileSystem;
 
 
 final class ProjectsPresenter extends Nette\Application\UI\Presenter
@@ -18,28 +19,32 @@ final class ProjectsPresenter extends Nette\Application\UI\Presenter
 	}
 
 	public function renderDefault() {
-		$this->template->projects = $this->database->table("projects")->fetchAll();
+		$this->template->projects = $this->database->table('projects')->fetchAll();
 	}
 
 	public function createComponentNewProject() {
 		$form = new Form;
-		$form->addText("name", "Name")->setRequired();
-		$form->addText("link", "Link")->setRequired();
-		$form->addText("path", "Path");
-		$form->addSubmit("submit", "Add Project");
-		$form->onSuccess[] = [$this, "newProjectDbWrite"];
+		$form->addText('name', 'Name')->setRequired('Add name');
+		$form->addText('link', 'Link')->setRequired('Add link');
+		$form->addMultiUpload('images', 'Images')->addRule($form::IMAGE, 'Select images');
+		$form->addUpload('readme', 'Readme')->addRule($form::IMAGE, 'Select markdown');
+		$form->addSubmit('submit', 'Add Project');
+		$form->onSuccess[] = [$this, 'formDbWrite'];
 		return $form;
 	}
 
-	public function newProjectDbWrite(array $formData) : void {
-		$this->database->table("projects")->insert([
-			"name" => $formData["name"],
-			"path" => $formData["path"],
-			"link" => $formData["link"]
+	public function formDbWrite(array $formData) : void {
+		$path = './projects/' . $formData['name'];
+		FileSystem::createDir($path);
+		$this->database->table('projects')->insert([
+			'name' => $formData['name'],
+			'link' => $formData['link'],
+			'path' => $path
 		]);
+		$this->redirect('Projects:');
 	}
 
 	public function renderSingle($id) {
-		$this->template->project = $this->database->table("projects")->get($id);
+		$this->template->project = $this->database->table('projects')->get($id);
 	}
 }

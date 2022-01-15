c3ff213fe82a4a97a887039a54749882e0cfd000
Jan Kornienko <kor.jan@outlook.com>
2022-01-15 16:42:42 +0100

    Skills Section

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,6 +5,16 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Database\Explorer;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter {
+	private Explorer $database;
+
+	public function __construct(Explorer $database) {
+		$this->database = $database;
+	}
+
+	public function renderDefault() {
+		$this->template->skills = $this->database->table('skills')->fetchAll();
+	}
 }

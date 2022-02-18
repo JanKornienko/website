7152dfa40f32c5cd55d42bf8198f45b338133a00
Jan Kornienko <kor.jan@outlook.com>
2022-02-18 22:57:20 +0100

    Project File Name

--- app/Presenters/ProjectsPresenter.php
+++ app/Presenters/ProjectsPresenter.php
@@ -38,7 +38,7 @@ final class ProjectsPresenter extends Nette\Application\UI\Presenter {
 		$path = '/files/projects/' . $projectFormData['name'] . '/';
 		FileSystem::createDir('.' . $path);
 		foreach ($projectFormData['images'] as $file) {
-			$name = '.' . $path . 'IMG_' . $file->name . '_' . uniqid();
+			$name = '.' . $path . 'IMG_' . $file->name;
 			$file->move($name);
 		}
 		$this->database->table('projects')->insert([
